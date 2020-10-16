afb073ec4f31d0a9b2722ccc362e58991f69b49f
Saul Meneses <fob2257@gmail.com>
2020-10-15 22:48:09 -0700

    Handling ReferenceLoop

--- NetCore3.1.API/Startup.cs
+++ NetCore3.1.API/Startup.cs
@@ -34,7 +34,10 @@ namespace NetCore3._1.API
                 options.UseSqlServer(connectionString);
             });
 
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(options =>
+            {
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
